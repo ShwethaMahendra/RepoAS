7c49a25a2f4d759464415c9da6443e4373e233f9
ShwethaMahendra <mmshwetha@gmail.com>
2025-03-13 16:15:07 +0530

    push

--- src/test/java/Demo/Practice.java
+++ src/test/java/Demo/Practice.java
@@ -4,9 +4,11 @@ import org.testng.annotations.Test;
 
 public class Practice {
 //pull
+	
 	@Test
 	private void m1() {
 		System.out.println("Hello");
+		//Modificatin & push to github to check it autoruns with scm (* * * * *)
 
 	}
 }
